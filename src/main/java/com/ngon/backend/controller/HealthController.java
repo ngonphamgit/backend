f2678b7869ba2f64f0ab8ccba80a21265da3b408
ngonphamgit <phamtngon@gmail.com>
2026-06-08 17:36:27 -0500

    Initial commit

---
+++ src/main/java/com/ngon/backend/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.ngon.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HealthController
+{
+    @GetMapping("/health")
+    public String GetHealth()
+    {
+        return "Healthy";
+    }
+}
